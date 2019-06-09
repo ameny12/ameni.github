8df2b6b0720b87d12748e93b2451dc22d6e85570
ameny12 <51385870+ameny12@users.noreply.github.com>
2019-06-09 03:19:47 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 // Generate random room name if needed
 if (!location.hash) {
-  location.hash =new Date().toFixed(16).substring(2);
+  location.hash =(new Date()).getTime();
 }
 const roomHash = location.hash.substring(1);
 
