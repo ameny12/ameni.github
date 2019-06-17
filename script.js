91f692c66a50a0a4c0f883132819a8722dcf29b2
ameny12 <51385870+ameny12@users.noreply.github.com>
2019-06-17 20:21:12 +0200

    Update script.js

--- script.js
+++ script.js
@@ -98,7 +98,12 @@ function startWebRTC(isOfferer) {
       pc.setRemoteDescription(new RTCSessionDescription(message.sdp), () => {
         // When receiving an offer lets answer it
         if (pc.remoteDescription.type === 'offer') {
-          pc.createAnswer(voiceActivityDetection: False).then(localDescCreated).catch(onError);
+          pc.createAnswer({ 
+obligatoire: { 
+OfferToReceiveVideo: false, 
+OfferToReceiveAudio: false
+} 
+}) .then(localDescCreated).catch(onError);
         }
       }, onError);
     } else if (message.candidate) {
