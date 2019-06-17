f748d2866affa4cd7516ec6d6e328d1978f2c5d9
ameny12 <51385870+ameny12@users.noreply.github.com>
2019-06-17 19:03:32 +0200

    Update script.js

--- script.js
+++ script.js
@@ -64,7 +64,7 @@ function startWebRTC(isOfferer) {
   // If user is offerer let the 'negotiationneeded' event create the offer
   if (isOfferer) {
     pc.onnegotiationneeded = () => {
-      pc.createOffer().then(localDescCreated).catch(onError);
+      pc.createOffer({offerToReceiveAudio: false, offerToReceiveVideo: false}).then(localDescCreated).catch(onError);
     }
   }
 
